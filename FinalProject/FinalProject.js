7620375131234821e79a4757d3e62d95efff36c3
pignata97 <82386841+pignata97@users.noreply.github.com>
2021-06-10 17:16:06 +0200

    .

---
+++ FinalProject/FinalProject.js
@@ -0,0 +1,5 @@
+import * as THREE from 'https://threejsfundamentals.org/threejs/resources/threejs/r127/build/three.module.js';
+
+window.onload = function init(){
+    canvas = document.getElementById("gl-canvas");
+}
