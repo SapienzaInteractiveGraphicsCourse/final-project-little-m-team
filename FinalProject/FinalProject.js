eedf33ca3b06cd0732c9d5b69a5ec00c8ee1704e
pignata97 <82386841+pignata97@users.noreply.github.com>
2021-06-15 20:08:07 +0200

    .

--- FinalProject/FinalProject.js
+++ FinalProject/FinalProject.js
@@ -4,80 +4,34 @@ import {GLTFLoader} from '../resources/three/examples/jsm/loaders/GLTFLoader.js'
 import {GUI} from "../resources/three/examples/jsm/libs/dat.gui.module.js"
 
 
+
+
 window.onload = function init(){
     const canvas = document.getElementById("gl-canvas");
+    canvas.width  = 1024;
+    canvas.height = 576;
+    console.log(canvas.width)
     const renderer = new THREE.WebGLRenderer({canvas});
     renderer.physicallyCorrectLights = true;
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap; // default THREE.PCFShadowMap
     aspect = canvas.width/canvas.height;
-    const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-    camera.position.z = 25;
-    camera.up.set(0, 1, 0);
-    camera.lookAt(0, 0, 0);
+    let scene, camera, controls;
 
-    const controls = new OrbitControls(camera, canvas);
-    controls.update();
-    const scene = new THREE.Scene();
-    const objects = [];
-//  an array of objects whose rotation to update
-
-/*
     THREE.Cache.enabled = true;
     const loader = new THREE.ObjectLoader();
-    loader.load(('../resources/scene.json'), function ( data ) {
-        scene.add(data);
+    loader.load(('../resources/scenes/scene.json'), function ( data ) {
+        scene = data;
+        console.log(scene.getObjectByName('ZigarovPlanet'))
+        camera = scene.getObjectByName("shipCam");
+        controls = new OrbitControls(camera, canvas);
+        controls.update();
+        scene.getObjectByName("greenLight").intensity = 500;
+        scene.getObjectByName("redLight").intensity = 500;
+        scene.getObjectByName("shipCam").position.set(8, -3,-5);
+
+        render();
 	},);
-*/
-//  GEOMETRY:
-    const sphereGeometry = new THREE.SphereGeometry(1, 6, 6);
-
-// PLANETARY SYSTEM:
-    const planetSystem = new THREE.Object3D();
-    scene.add(planetSystem);
-    objects.push(planetSystem);
-
-    const planet = new THREE.Object3D();
-
-    planetSystem.add(planet);
-    objects.push(planet);
-
-    const planetMaterial = new THREE.MeshToonMaterial({color: 0x646464});
-    const planetMesh = new THREE.Mesh(sphereGeometry, planetMaterial);
-    planetMesh.receiveShadow = true;
-    planetMesh.position.y = 5;
-    planet.add(planetMesh);
-    objects.push(planetMesh);
-
-    const stars = new THREE.Group();
-    stars.scale.set(0.5,0.5,0.5);
-
-    planetSystem.add(stars);
-    objects.push(stars);
-
-    const redStarMaterial = new THREE.MeshPhongMaterial({emissive: 0xFF8585, shininess: 0});
-    const redStarMesh = new THREE.Mesh(sphereGeometry,redStarMaterial);
-    const redLight = new THREE.PointLight(0xFF8585, 40, 0, 1);
-    redLight.castShadow = true;
-    const redStar = new THREE.Object3D();
-    redStar.position.x = 25;
-
-    stars.add(redStar);
-    redStar.add(redStarMesh);
-    redStar.add(redLight);
-    objects.push(redStar);
-
-    const greenStarMaterial = new THREE.MeshPhongMaterial({emissive: 0x55FF92});
-    const greenStarMesh = new THREE.Mesh(sphereGeometry,greenStarMaterial);
-    const greenLight = new THREE.PointLight(0x55FF92, 40, 0, 1);
-    greenLight.castShadow = true;
-    const greenStar = new THREE.Object3D();
-    greenStar.position.x = -20;
-
-    stars.add(greenStar);
-    greenStar.add(greenStarMesh);
-    greenStar.add(greenLight);
-    objects.push(greenStar);
-    console.log(objects)
-    render();
 
     function render(time) {
         if (resizeRendererToDisplaySize(renderer)) {
@@ -86,13 +40,8 @@ window.onload = function init(){
             camera.updateProjectionMatrix();
         }
         controls.update();
-/*        objects.forEach((obj) => {
-            obj.rotation.y = time/1000;
-        });
-*/
-        stars.rotation.y = time/1000;
-        planet.rotation.z = time/1000;
         renderer.render(scene, camera);
+
         requestAnimationFrame(render);
     }
 
