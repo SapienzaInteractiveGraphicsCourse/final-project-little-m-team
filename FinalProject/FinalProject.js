05cd5b13d449089108e035edca0fed2be5da3599
pignata97 <82386841+pignata97@users.noreply.github.com>
2021-06-16 06:17:01 +0200

    Update FinalProject.js

--- FinalProject/FinalProject.js
+++ FinalProject/FinalProject.js
@@ -3,6 +3,14 @@ import { OrbitControls } from '../resources/three/examples/jsm/controls/OrbitCon
 import {GLTFLoader} from '../resources/three/examples/jsm/loaders/GLTFLoader.js';
 import {GUI} from "../resources/three/examples/jsm/libs/dat.gui.module.js"
 
+function loadScene(){
+    THREE.Cache.enabled = true;
+    const loader = new THREE.ObjectLoader();
+    loader.load(('../resources/scenes/scene.json'), function (scene) {init(scene)});
+}
+
+window.onload = loadScene();
+
 class ShipController {
     constructor(shipObject) {
         this.ship = shipObject.getObjectByName("shipCam");
@@ -10,79 +18,101 @@ class ShipController {
         this.speed = 0;
         this.incline = 0;
         this.flip = 0;
-        this.forward = shipObject.getWorldDirection();
-        console.log(this.forward);
-        this.controls = {
-            38: 'speedUp',      // Up   Arrow
-            40: 'speedDown',    // Down Arrow
-            37: 'turnLeft',     // Left Arrow
-            39: 'turnRight',    // Right Arrow
 
-            87: 'inclineUp',    // W
-            83: 'inclineDown',  // S
-            65: 'flipLeft',     // A
-            68: 'flip Riight',  // D
+        this.forward = shipObject.getWorldDirection();
+        this.engine = false;
+        this.light.intensity = 0;
+
+        this.controls = {
+            'speedUp': 38,      // UpArrow
+            'speedDown': 40,    // DownArrow
+            'turnLeft': 37,     // LeftArrow
+            'turnRight': 39,    // RightArrow
+
+            'inclineUp': 87,    // W
+            'inclineDown': 83,  // S
+            'flipLeft': 65,     // A
+            'flip Riight': 68,  // D
+            'engine': 16,       //ShiftRight
         };
+
+        window.addEventListener('keydown', (e) => {
+            switch(e.keyCode){
+                case (this.controls['speedUp']):
+                    if (this.engine && this.speed <0.2) this.speed += 0.05;
+                    console.log(this.speed);
+                break;
+                case (this.controls['speedDown']):
+                    if (this.engine && this.speed >-0.2) this.speed -= 0.05;
+                    console.log(this.speed);
+                break;
+                case (this.controls['inclineUp']):
+                    this.incline += 1;
+                    console.log(this.incline);
+                break;
+                case (this.controls['inclineDown']):
+                    this.incline -= 1;
+                    console.log(this.incline);
+                break;
+
+                case (this.controls['flipLeft']):
+                    this.flip -= 1;
+                    console.log(this.flip);
+                break;
+                case (this.controls['flipRight']):
+                    this.flip +=1;
+                    console.log(this.flip);
+                break;
+                case this.controls['engine']:
+                    this.engine = !this.engine;
+                    this.speed = 0;
+                    this.light.intensity = this.engine;
+                    console.log(this.engine);
+                break;
+                default:
+                    console.log(e.keyCode);
+            }
+        });
     }
+
 
     update(){
-        console.log(this.speed,this.incline,this.flip)
-        //this.ship.translateOnAxis(this.forward.normalize(), this.speed);
+        if (this.engine){
+            this.light.intensity = 1 + Math.abs(this.speed)*10;
+            this.ship.translateOnAxis(this.forward.normalize(), this.speed);
+        }
     }
+
 };
 
-window.onload = function init(){
+
+function init(scene){
     const canvas = document.getElementById("gl-canvas");
     canvas.width  = 1024;
     canvas.height = 576;
+
     const renderer = new THREE.WebGLRenderer({canvas});
     renderer.physicallyCorrectLights = true;
     renderer.shadowMap.enabled = true;
     renderer.shadowMap.type = THREE.PCFSoftShadowMap; // default THREE.PCFShadowMap
-    aspect = canvas.width/canvas.height;
-    let scene, camera, controls, controller;
 
-    THREE.Cache.enabled = true;
-    const loader = new THREE.ObjectLoader();
-    loader.load(('../resources/scenes/scene.json'), function ( data ) {
-        scene = data;
-        camera = scene.getObjectByName("shipCam");
-        controls = new OrbitControls(camera, canvas);
-        controls.update();
+    const camera = scene.getObjectByName("shipCam");
+    camera.position.set(0,0,15);
+    camera.lookAt(0,0,0);
+    const controls = new OrbitControls(camera, canvas);
+    controls.update();
+    const controller = new ShipController(camera);
+    console.log(controller.ship)
+    modifyScene();
+
+
+    function modifyScene(){
         scene.getObjectByName("greenLight").intensity = 500;
         scene.getObjectByName("redLight").intensity = 500;
-        scene.getObjectByName("shipCam").position.set(8, -3,-5);
-        controller = new ShipController(scene.getObjectByName("shipCam"));
 
-        canvas.addEventListener('keydown', (e) => {
+    }
 
-            switch(e.keycode){
-                case (controller.controls['speedUp']):
-                controller.speed += 0.1;
-                break;
-                case (controller.controls['speedDown']):
-                controller.speed -= 0.1;
-                break;
-                case (controller.controls['inclineUp']):
-                controller.incline += 1;
-                break;
-                case (controller.controls['inclineDown']):
-                controller.incline -= 1;
-                break;
-
-                case (controller.controls['flipLeft']):
-                controller.flip -= 1;
-                break;
-                case (controller.controls['flipRight']):
-                controller.flip +=1;
-                break;
-                default: console.log(e.keycode);
-                break;
-            }
-        });
-        render();
-	});
-
+    render();
 
     function render(time) {
         if (resizeRendererToDisplaySize(renderer)) {
