7eb2b16c8756a4d7792bd786bcb5fdb2436b1234
pignata97 <82386841+pignata97@users.noreply.github.com>
2021-06-18 22:48:08 +0200

    .

--- FinalProject/FinalProject.js
+++ FinalProject/FinalProject.js
@@ -13,21 +13,24 @@ window.onload = loadScene();
 
     class ShipController {
         constructor(shipObject) {
-            this.ship = shipObject.getObjectByName("shipCam");
+            this.ship = shipObject.getObjectByName("ship");
+            this.cam = shipObject.getObjectByName("shipCam");
             this.light = shipObject.getObjectByName("shipLight");
             this.gear = 4;
             this.speed = [-0.2, -0.1, -0.05, -0.01, 0, 0.01, 0.05, 0.1, 0.2];
             this.incline = 0;
             this.flip = 0;
             this.engine = false;
+            this.pov = true;
             this.light.intensity = 0;
-
+            this.ship.position.set(0,-1,-2);
             this.forward = new THREE.Vector3();
             shipObject.getWorldDirection(this.forward);
+            console.log(this.forward);
             this.forward = this.forward.normalize();
 
             let x = this.forward.clone();
-            x.cross(this.ship.up);
+            x.cross(this.cam.up);
             this.cross = x.normalize();
 
             this.controls = {
@@ -48,25 +51,28 @@ window.onload = loadScene();
                 switch(e.keyCode){
                     case (this.controls['speedUp']):
                         if (this.engine && this.speed[this.gear] < 0.2) this.gear +=1;
-                        console.log(this.speed[this.gear]);
                     break;
                     case (this.controls['speedDown']):
                         if (this.engine && this.gear > 0) this.gear --;
                         console.log(this.speed[this.gear]);
                     break;
                     case (this.controls['inclineUp']):
-                        this.ship.rotateOnAxis(this.cross, 0.02);
+                        if (this.engine) this.cam.rotateOnAxis(this.cross, 0.02);
                     break;
                     case (this.controls['inclineDown']):
-                        this.ship.rotateOnAxis(this.cross, -0.02);
+                        if (this.engine) this.cam.rotateOnAxis(this.cross, -0.02);
                     break;
 
                     case (this.controls['flipLeft']):
-                        this.ship.rotateOnAxis(this.forward, -0.05);
+                        if (this.engine) {
+                            this.cam.rotateOnAxis(this.forward, -0.05);
+                            this.cam.updateProjectionMatrix();
+                        }
+
 
                     break;
                     case (this.controls['flipRight']):
-                        this.ship.rotateOnAxis(this.forward, 0.05);
+                        if (this.engine) this.cam.rotateZ(-0.05);
                     break;
                     case this.controls['engine']:
                         this.engine = !this.engine;
@@ -75,7 +81,13 @@ window.onload = loadScene();
                         console.log(this.engine);
                     break;
                     case this.controls['reset']:
-                        this.ship.lookAt(0,0,0);
+                        this.cam.lookAt(0,0,0);
+                    case 86:
+                        this.switchPOV();
+                    break;
+                    case 32:
+                        this.landing();
+                    break;
                     default:
                         console.log(e.keyCode);
                 }
@@ -86,10 +98,38 @@ window.onload = loadScene();
         update(){
             if (this.engine){
                 this.light.intensity = (1 + Math.abs(this.speed[this.gear])*10);
-                this.ship.translateOnAxis(this.forward, this.speed[this.gear]);
+                this.cam.translateOnAxis(this.forward, this.speed[this.gear]);
             }
         }
 
+        landing(){
+            let p = new THREE.Vector3();
+            this.cam.getWorldDirection(this.forward);
+            this.cam.getWorldPosition(p);
+            p.negate().normalize();
+            const phi = p.angleTo(this.forward);
+            const w = new THREE.Vector3().crossVectors(this.forward,p);
+            this.cam.rotateOnAxis(w, phi)
+            this.switchPOV();
+
+        //    const theta = p.angleTo(this.up)
+
+
+            //console.log(w)
+        }
+
+        switchPOV(){
+
+            if (this.pov){
+                this.ship.rotateX(-1.57);
+                this.ship.translateY(-1);
+            }
+            else {
+                this.ship.translateY(1);
+                this.ship.rotateX(1.57);
+            }
+            this.pov = !this.pov;
+        }
     };
     class ColorGUIHelper {
         constructor(object, prop) {
@@ -116,34 +156,14 @@ function init(scene){
     renderer.shadowMap.type = THREE.PCFSoftShadowMap; // default THREE.PCFShadowMap
 
     const camera = scene.getObjectByName("shipCam");
-    camera.position.set(0,0,40);
+    camera.position.set(0,0,20);
     camera.lookAt(0,0,0);
     const controller = new ShipController(camera);
     const lights = [scene.getObjectByName("redLight"), scene.getObjectByName("greenLight"), scene.getObjectByName("ambientLight")];
     lights[2].visible = true;
-    console.log(lights[2])
     modifyScene();
-
-    function modifyScene(){
-        lights.forEach(light => {
-            console.log(light);
-            light.power = 5000;
-            light.decay = 1;
-            light.distance = Infinity;
-        });
-        scene.getObjectByName("ZigarovPlanet").scale.set(10,10,10);
-
-    }
-
-    const gui = new GUI();
-    const lightsFolder = gui.addFolder("Lights");
-    lights.forEach(light => {
-        lightsFolder.addColor(new ColorGUIHelper(light, 'color'), 'value').name('color');
-        lightsFolder.add(light, 'power', 1000, 10000);
-    });
-
-    const camFolder = gui.addFolder('Camera');
-    camFolder.add(camera, 'fov', 30, 120);
+    options();
+    //controller.landing();
     render();
 
     function render(time) {
@@ -155,11 +175,33 @@ function init(scene){
         orbits(time*0.001);
         camera.updateProjectionMatrix();
         controller.update();
+
         renderer.render(scene, camera);
 
         requestAnimationFrame(render);
     }
 
+    function modifyScene(){
+        lights.forEach(light => {
+            light.power = 5000;
+            light.decay = 1;
+            light.distance = Infinity;
+        });
+        scene.getObjectByName("ZigarovPlanet").scale.set(10,10,10);
+
+    }
+
+    function options(){
+        const gui = new GUI();
+        const lightsFolder = gui.addFolder("Lights");
+        lights.forEach(light => {
+            lightsFolder.addColor(new ColorGUIHelper(light, 'color'), 'value').name('color');
+            lightsFolder.add(light, 'power', 1000, 10000);
+        });
+
+        const camFolder = gui.addFolder('Camera');
+        camFolder.add(camera, 'fov', 30, 120);
+    }
     function orbits(t){
         scene.getObjectByName("Stars").rotation.y = t*0.1;
         scene.getObjectByName("ZigarovPlanet").rotation.y = -t*0.01;
