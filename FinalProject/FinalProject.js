b62549a65893bc97aeeeed6b9ba129caa09edfc4
pignata97 <82386841+pignata97@users.noreply.github.com>
2021-08-25 16:12:04 +0200

    Setup to develop animation

--- FinalProject/FinalProject.js
+++ FinalProject/FinalProject.js
@@ -6,206 +6,10 @@ import {GUI} from "../resources/three/examples/jsm/libs/dat.gui.module.js"
 function loadScene(){
     THREE.Cache.enabled = false;
     const loader = new THREE.ObjectLoader();
-    loader.load(('../resources/scenes/newScenee.json'), function (scene) {init(scene)});
+    loader.load(('scenes/characterAnimation.json'), function (scene) {init(scene)});
 }
 
 window.onload = loadScene();
-
-    class SpaceShip {
-        constructor(shipObject) {
-            this.model = shipObject;
-            this.cam =  shipObject.getObjectByName("shipCam");
-            this.lights = shipObject.getObjectByName("shipLights");
-
-            this.lights.children[1].visible = true;
-            this.lights.children[1].intensity = 0.0;
-            this.lights.children[0].intensity = 10.0;
-
-            this.engine = false;
-            this.gear = 4,
-            this.speed = [-0.2, -0.1, -0.05, -0.01, 0, 0.01, 0.05, 0.1, 0.2]
-
-            this.rolling = 0;
-            this.pitching = 0;
-            this.camAngle = 0;
-
-            this.fw = new THREE.Vector3();  // Flip Axis of Rotation
-            this.u = new THREE.Vector3();
-            this.w = new THREE.Vector3();   // Incline Axis of Rotation
-            this.ray = new THREE.Raycaster();
-            this.updateAxis();
-
-            this.collision = false;
-            this.landing = false;
-        }
-
-        updateAxis() {
-            var front = new THREE.Vector3();
-            var back = new THREE.Vector3();
-            var left = new THREE.Vector3();
-            var right = new THREE.Vector3();
-
-            this.lights.getObjectByName("frontLight").getWorldPosition(front);
-            this.lights.getObjectByName("engineLight").getWorldPosition(back);
-            this.lights.getObjectByName("leftLight").getWorldPosition(left);
-            this.lights.getObjectByName("rightLight").getWorldPosition(right);
-
-            this.fw = front.clone().add(back.negate()).normalize();
-            this.w = right.clone().add(left.negate()).normalize();
-            this.u.crossVectors(this.w,this.fw);
-            this.ray.set(front,this.fw);
-        }
-        checkCollision(cast){
-            if((typeof cast !== 'undefined')&&(cast.distance <2)&&(!this.collision)){
-                console.log("Collision Detected");
-                this.collision = true;
-                this.gear = 4;
-                this.lights.children[1].intensity = this.engine;
-            }
-            else if((typeof cast !== 'undefined')&&(cast.distance >2)&&(this.collision)){
-                this.collision = false;
-            }
-        }
-
-        land() {
-            if (this.engine) {
-                if (this.landed){
-                    this.model.translateOnAxis(this.u,2);
-                    this.landed = false;
-                }
-
-                else{
-                    var p = new THREE.Vector3();
-                    this.model.getWorldPosition(p);
-                    const d = p.length() - 10;
-
-                    if (d<3){
-                        const phi = p.angleTo(this.u);
-                        this.model.rotateOnWorldAxis(this.w,phi);
-                        this.model.translateOnAxis(this.u.negate(),d);
-                        this.ssEngine();
-                        this.landed = true;
-                    }
-                    else console.log("Too far for landing: ",d);
-                }
-            }
-        }
-
-
-        update(cast){
-                this.checkCollision(cast);
-                if (this.landing) this.land();
-                else{
-                    if (this.rolling != 0){
-                        this.roll(this.rolling);
-                        if(-0.2 < this.camAngle && this.camAngle  < 0.2){
-                            this.cam.rotateZ(-this.rolling/2);
-                            this.camAngle -= this.rolling/2;
-                        }
-                    }
-                    else if (Math.abs(this.camAngle) > 0.01){
-                        this.cam.rotateZ(-Math.sign(this.camAngle)*0.02);
-                        this.camAngle -= Math.sign(this.camAngle)*0.02;
-                    }
-                    if (this.pitching != 0){
-                        this.pitch(this.pitching);
-                    }
-                    this.model.translateY(-this.speed[this.gear]);
-                }
-                this.updateAxis();
-        }
-
-        ssEngine() {
-            this.engine = !this.engine;
-            console.log("Engine On: ",this.engine);
-            this.gear = 4;
-            this.lights.children[1].intensity = this.engine;
-        }
-
-        shiftUp(){
-            if (this.engine && this.gear < 8 && !this.landed) {
-                this.gear +=1;
-                this.lights.children[1].intensity += (2)*Math.sign(this.speed[this.gear]);
-            }
-        }
-        shiftDown(){
-            if (this.engine && this.gear > 0 && !this.landed) {
-                this.gear -=1;
-                this.lights.children[1].intensity -= 2*Math.sign(this.speed[this.gear]);
-            }
-        }
-        roll(theta){
-            if (!this.landed) this.model.rotateY(theta);
-        }
-        pitch(phi){
-            if (!this.landed) this.model.rotateX(phi);
-        }
-        dodge(theta){}
-        reset(){}
-
-        setUp(){
-            window.addEventListener('keydown', (e) => {
-                switch(e.code){
-                    case "ShiftRight": this.ssEngine();               // Shift
-                    break;
-                    case "ArrowUp": this.shiftUp();                // Up Arrow
-                    break;
-                    case "ArrowDown": this.shiftDown();              // Down Arrow
-                    break;
-                    case "KeyW":                                // W
-                        if (this.engine && this.pitching > -0.02){
-                            this.pitching = -0.02;
-                        }
-                    break;
-                    case "KeyS":                                // S
-                        if (this.engine && this.pitching < 0.02){
-                            this.pitching = 0.02;
-                        }
-                    break;
-                    case "KeyA":                                // A
-                        if (this.engine && this.rolling < 0.05){
-                            this.rolling = 0.05;
-                            //this.cam.rotateZ(-0.2);
-                        }
-                    break;
-                    case "KeyD":                                // D
-                        if (this.engine && this.rolling > -0.05){
-                            this.rolling = -0.05;
-                            //this.cam.rotateZ(0.2);
-                        }
-                    break;
-                    case "ArrowLeft": this.dodge(-90);
-                    break;
-                    case "ArrowRight": this.dodge(90);
-                    break;
-                    case "KeyQ": this.land();
-                    break;
-                    case "Backspace": this.reset();
-                    break;
-                }
-            });
-            window.addEventListener('keyup', (e) => {
-                switch(e.code){
-                    case "KeyW": this.pitching = 0;
-                    break;
-                    case "KeyS": this.pitching = 0;
-                    break;
-                    case "KeyA":
-                        if(this.rolling > 0){
-                            //this.cam.rotateZ(0.2);
-                            this.rolling = 0;
-                        }
-                    break;
-                    case "KeyD":
-                        if(this.rolling < 0){
-                            //this.cam.rotateZ(-0.2);
-                            this.rolling = 0;
-                        }
-                    break;
-                }
-            });
-        }
-    }
 
     class ColorGUIHelper {
         constructor(object, prop) {
@@ -227,16 +31,23 @@ function init(scene){
     canvas.height = 576;
 
     const renderer = new THREE.WebGLRenderer({canvas});
-    renderer.physicallyCorrectLights = true;
+    //renderer.physicallyCorrectLights = true;
     renderer.shadowMap.enabled = true;
     renderer.shadowMap.type = THREE.PCFSoftShadowMap; // default THREE.PCFShadowMap
 
-    const player = new SpaceShip(scene.getObjectByName("Ship"));
-    player.setUp();
+    const camera = new THREE.PerspectiveCamera( 50, canvas.width / canvas.height, 0.01, 1000);
+    camera.position.set(0,1,3);
+    camera.rotateX(-0.2);
+    scene.add(camera);
 
-    const camera = player.cam;
-    const lights = [scene.getObjectByName("star1Light"), scene.getObjectByName("star2Light")];
-    guiOptions();
+    const light = new THREE.PointLight();
+    light.position.set(0, 0, 1);
+    scene.add(light);
+
+    const player = scene.getObjectByName("myAstronaut");
+    console.log(player)
+
+    //guiOptions();
     render();
 
     function render(time) {
@@ -245,32 +56,23 @@ function init(scene){
             camera.aspect = canvas.clientWidth / canvas.clientHeight;
             camera.updateProjectionMatrix();
         }
+        /*
         orbits(time*0.001);
         camera.updateProjectionMatrix();
         var cast = new Array();
         scene.getObjectByName("PlanetZigarov").raycast(player.ray,cast);
         //if (cast.length >0) console.log(cast[0].distance.toPrecision(3));
         player.update(cast[0]);
+        */
+
         renderer.render(scene, camera);
         requestAnimationFrame(render);
     }
 
     function guiOptions(){
         const gui = new GUI();
-        const lightsFolder = gui.addFolder("Lights");
-        lights.forEach(light => {
-            lightsFolder.addColor(new ColorGUIHelper(light, 'color'), 'value').name('color');
-            lightsFolder.add(light, 'intensity', 10, 1000);
-        });
-
         const camFolder = gui.addFolder('Camera');
         camFolder.add(camera, 'fov', 30, 120);
-    }
-
-    function orbits(t){
-        scene.getObjectByName("Stars").rotation.y = t*0.1;
-        scene.getObjectByName("PlanetZigarov").rotation.y = -t*0.01;
-        scene.getObjectByName("Universe").rotation.y = t*0.01;
     }
 
     function resizeRendererToDisplaySize(renderer) {
