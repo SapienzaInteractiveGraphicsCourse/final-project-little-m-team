61465f1521d199074b84f89896345b63b5631eca
pignata97 <82386841+pignata97@users.noreply.github.com>
2021-08-27 16:24:50 +0200

    .

--- FinalProject/Data.js
+++ FinalProject/Data.js
@@ -1,32 +1,32 @@
 const c = Math.PI/180;
 
 export const Data = {
-    astronaut: {
-        joints: [8, 51, 53, 55, 58, 60, 62, 18, 20, 30, 32],
+    'Astronaut': {
+        joints: ['Astronaut', 'rightHip', 'rightKnee', 'rightAnkle', 'leftHip', 'leftKnee', 'leftAnkle', 'rightShoulder', 'rightElbow', 'leftShoulder', 'leftElbow'],
         animations: {
             'Walk': {
                 frames : [
-                    [ {y: 0.47}, {y: 0.51}, {y: 0.55}, {y: 0.57},       {y: 0.47}, {y: 0.51}, {y: 0.55}, {y: 0.57} ],       // Height
+                    [ {y: 10.47}, {y: 10.51}, {y: 10.55}, {y: 10.57},       {y: 10.47}, {y: 10.51}, {y: 10.55}, {y: 10.57} ],       // Height
 
-                    [ {x: 15*c}, {x: 0*c}, {x: -30*c}, {x: -60*c},      {x: -35*c}, {x: -30*c}, {x: 5*c}, {x: 15*c} ],      // rightLeg.Anca
-                    [ {x: 30*c}, {x: 90*c}, {x: 120*c}, {x: 90*c},      {x: 0*c}, {x: 45*c}, {x: 5*c}, {x: 0*c} ],          // rightLeg.Ginocchio
-                    [ {y: 40*c}, {y: 0*c}, {y: 30*c}, {y: 0*c},         {y: 0*c}, {y: 15*c}, {y: 10*c}, {y: -15*c} ],       // rightLeg.Caviglia
+                    [ {x: 15*c}, {x: 0*c}, {x: -30*c}, {x: -60*c},      {x: -35*c}, {x: -30*c}, {x: 5*c}, {x: 15*c} ],      // rightHip
+                    [ {x: 30*c}, {x: 90*c}, {x: 120*c}, {x: 90*c},      {x: 0*c}, {x: 45*c}, {x: 5*c}, {x: 0*c} ],          // rightKnee
+                    [ {y: 40*c}, {y: 0*c}, {y: 30*c}, {y: 0*c},         {y: 0*c}, {y: 15*c}, {y: 10*c}, {y: -15*c} ],       // rightAnkle
 
-                    [ {x: -35*c}, {x: -30*c}, {x: 5*c}, {x: 15*c},      {x: 15*c}, {x: 0*c}, {x: -30*c}, {x: -60*c} ],      // leftLeg.Anca
-                    [ {x: 0*c}, {x: 45*c}, {x: 5*c}, {x: 0*c},          {x: 30*c}, {x: 90*c}, {x: 120*c}, {x: 90*c} ],      // leftLeg.Ginocchio
-                    [ {y: 0*c}, {y: 15*c}, {y: 10*c}, {y: -15*c},       {y: 40*c}, {y: 0*c}, {y: 30*c}, {y: 0*c} ],         // leftLeg.Caviglia
+                    [ {x: -35*c}, {x: -30*c}, {x: 5*c}, {x: 15*c},      {x: 15*c}, {x: 0*c}, {x: -30*c}, {x: -60*c} ],      // leftHip
+                    [ {x: 0*c}, {x: 45*c}, {x: 5*c}, {x: 0*c},          {x: 30*c}, {x: 90*c}, {x: 120*c}, {x: 90*c} ],      // leftKnee
+                    [ {y: 0*c}, {y: 15*c}, {y: 10*c}, {y: -15*c},       {y: 40*c}, {y: 0*c}, {y: 30*c}, {y: 0*c} ],         // leftAnkle
 
-                    [ {x: -45*c}, {x: -60*c}, {x: 30*c}, {x: 45*c},     {x: 45*c}, {x: 60*c}, {x: 30*c}, {x: 30*c} ],       // rightArm.Spalla
-                    [ {x: -90*c}, {x: -90*c}, {x: -90*c}, {x: -75*c},   {x: -45*c}, {x: -45*c}, {x: -90*c}, {x: -90*c} ],   // rightArm.Gomito
+                    [ {x: -45*c}, {x: -60*c}, {x: 30*c}, {x: 45*c},     {x: 45*c}, {x: 60*c}, {x: 30*c}, {x: 30*c} ],       // rightShoulder
+                    [ {x: -90*c}, {x: -90*c}, {x: -90*c}, {x: -75*c},   {x: -45*c}, {x: -45*c}, {x: -90*c}, {x: -90*c} ],   // rightElbow
 
-                    [ {x: 45*c}, {x: 60*c}, {x: 30*c}, {x: 30*c},       {x: -45*c}, {x: -60*c}, {x: 30*c}, {x: 45*c} ],     // leftArm.Spalla
-                    [ {x: -45*c}, {x: -45*c}, {x: -90*c}, {x: -90*c},   {x: -90*c}, {x: -90*c}, {x: -90*c}, {x: -75*c} ]    // leftArm.Gomito
+                    [ {x: 45*c}, {x: 60*c}, {x: 30*c}, {x: 30*c},       {x: -45*c}, {x: -60*c}, {x: 30*c}, {x: 45*c} ],     // leftShoulder
+                    [ {x: -45*c}, {x: -45*c}, {x: -90*c}, {x: -90*c},   {x: -90*c}, {x: -90*c}, {x: -90*c}, {x: -75*c} ],   // leftElbow
                 ],
                 periods: [100]*8,
                 repeat: true
             },
             'Reset': {
-                frames: [[{y: 0.5}], [{x: 0}], [{x: 0}], [{y: 0}], [{x: 0}], [{x: 0}], [{y: 0}], [{x: 0}], [{x: 0}], [{x: 0}], [{x: 0}]],
+                frames: [[{y: 10.5}], [{x: 0}], [{x: 0}], [{y: 0}], [{x: 0}], [{x: 0}], [{y: 0}], [{x: 0}], [{x: 0}], [{x: 0}], [{x: 0}]],
                 periods: [300],
                 repeat: false
             }
--- FinalProject/FinalProject.js
+++ FinalProject/FinalProject.js
@@ -10,204 +10,11 @@ window.onload = loadScene();
 function loadScene(){
     THREE.Cache.enabled = false;
     const loader = new THREE.ObjectLoader();
-    loader.load(('scenes/characterAnimation.json'), function (scene) {init(scene)});
+    //loader.load(('scenes/PlanetSystem.json'), function (scene) {init(scene)});
+    loader.load(('scenes/CharacterAnimation.json'), function (scene) {init(scene)});
+
 }
 
-class SpaceShip {
-    constructor(shipObject) {
-        this.model = shipObject;
-        this.cam =  shipObject.getObjectByName("shipCam");
-        this.lights = shipObject.getObjectByName("shipLights");
-
-        this.lights.children[1].visible = true;
-        this.lights.children[1].intensity = 0.0;
-        this.lights.children[0].intensity = 10.0;
-
-        this.engine = false;
-        this.gear = 4,
-        this.speed = [-0.2, -0.1, -0.05, -0.01, 0, 0.01, 0.05, 0.1, 0.2]
-
-        this.rolling = 0;
-        this.pitching = 0;
-        this.camAngle = 0;
-
-        this.fw = new THREE.Vector3();  // Flip Axis of Rotation
-        this.u = new THREE.Vector3();
-        this.w = new THREE.Vector3();   // Incline Axis of Rotation
-        this.ray = new THREE.Raycaster();
-        this.updateAxis();
-
-        this.collision = false;
-        this.landing = false;
-    }
-
-    updateAxis() {
-        var front = new THREE.Vector3();
-        var back = new THREE.Vector3();
-        var left = new THREE.Vector3();
-        var right = new THREE.Vector3();
-
-        this.lights.getObjectByName("frontLight").getWorldPosition(front);
-        this.lights.getObjectByName("engineLight").getWorldPosition(back);
-        this.lights.getObjectByName("leftLight").getWorldPosition(left);
-        this.lights.getObjectByName("rightLight").getWorldPosition(right);
-
-        this.fw = front.clone().add(back.negate()).normalize();
-        this.w = right.clone().add(left.negate()).normalize();
-        this.u.crossVectors(this.w,this.fw);
-        this.ray.set(front,this.fw);
-    }
-    checkCollision(cast){
-        if((typeof cast !== 'undefined')&&(cast.distance <2)&&(!this.collision)){
-            console.log("Collision Detected");
-            this.collision = true;
-            this.gear = 4;
-            this.lights.children[1].intensity = this.engine;
-        }
-        else if((typeof cast !== 'undefined')&&(cast.distance >2)&&(this.collision)){
-            this.collision = false;
-        }
-    }
-
-    land() {
-        if (this.engine) {
-            if (this.landed){
-                this.model.translateOnAxis(this.u,2);
-                this.landed = false;
-            }
-
-            else{
-                var p = new THREE.Vector3();
-                this.model.getWorldPosition(p);
-                const d = p.length() - 10;
-
-                if (d<3){
-                    const phi = p.angleTo(this.u);
-                    this.model.rotateOnWorldAxis(this.w,phi);
-                    this.model.translateOnAxis(this.u.negate(),d);
-                    this.ssEngine();
-                    this.landed = true;
-                }
-                else console.log("Too far for landing: ",d);
-            }
-        }
-    }
-
-
-    update(cast){
-            this.checkCollision(cast);
-            if (this.landing) this.land();
-            else{
-                if (this.rolling != 0){
-                    this.roll(this.rolling);
-                    if(-0.2 < this.camAngle && this.camAngle  < 0.2){
-                        this.cam.rotateZ(-this.rolling/2);
-                        this.camAngle -= this.rolling/2;
-                    }
-                }
-                else if (Math.abs(this.camAngle) > 0.01){
-                    this.cam.rotateZ(-Math.sign(this.camAngle)*0.02);
-                    this.camAngle -= Math.sign(this.camAngle)*0.02;
-                }
-                if (this.pitching != 0){
-                    this.pitch(this.pitching);
-                }
-                this.model.translateY(-this.speed[this.gear]);
-            }
-            this.updateAxis();
-    }
-
-    ssEngine() {
-        this.engine = !this.engine;
-        console.log("Engine On: ",this.engine);
-        this.gear = 4;
-        this.lights.children[1].intensity = this.engine;
-    }
-
-    shiftUp(){
-        if (this.engine && this.gear < 8 && !this.landed) {
-            this.gear +=1;
-            this.lights.children[1].intensity += (2)*Math.sign(this.speed[this.gear]);
-        }
-    }
-    shiftDown(){
-        if (this.engine && this.gear > 0 && !this.landed) {
-            this.gear -=1;
-            this.lights.children[1].intensity -= 2*Math.sign(this.speed[this.gear]);
-        }
-    }
-    roll(theta){
-        if (!this.landed) this.model.rotateY(theta);
-    }
-    pitch(phi){
-        if (!this.landed) this.model.rotateX(phi);
-    }
-    dodge(theta){}
-    reset(){}
-
-    setUp(){
-        window.addEventListener('keydown', (e) => {
-            switch(e.code){
-                case "ShiftRight": this.ssEngine();               // Shift
-                break;
-                case "ArrowUp": this.shiftUp();                // Up Arrow
-                break;
-                case "ArrowDown": this.shiftDown();              // Down Arrow
-                break;
-                case "KeyW":                                // W
-                    if (this.engine && this.pitching > -0.02){
-                        this.pitching = -0.02;
-                    }
-                break;
-                case "KeyS":                                // S
-                    if (this.engine && this.pitching < 0.02){
-                        this.pitching = 0.02;
-                    }
-                break;
-                case "KeyA":                                // A
-                    if (this.engine && this.rolling < 0.05){
-                        this.rolling = 0.05;
-                        //this.cam.rotateZ(-0.2);
-                    }
-                break;
-                case "KeyD":                                // D
-                    if (this.engine && this.rolling > -0.05){
-                        this.rolling = -0.05;
-                        //this.cam.rotateZ(0.2);
-                    }
-                break;
-                case "ArrowLeft": this.dodge(-90);
-                break;
-                case "ArrowRight": this.dodge(90);
-                break;
-                case "KeyQ": this.land();
-                break;
-                case "Backspace": this.reset();
-                break;
-            }
-        });
-        window.addEventListener('keyup', (e) => {
-            switch(e.code){
-                case "KeyW": this.pitching = 0;
-                break;
-                case "KeyS": this.pitching = 0;
-                break;
-                case "KeyA":
-                    if(this.rolling > 0){
-                        //this.cam.rotateZ(0.2);
-                        this.rolling = 0;
-                    }
-                break;
-                case "KeyD":
-                    if(this.rolling < 0){
-                        //this.cam.rotateZ(-0.2);
-                        this.rolling = 0;
-                    }
-                break;
-            }
-        });
-    }
-}
 
 class ColorGUIHelper {
     constructor(object, prop) {
@@ -229,120 +36,26 @@ function init(scene){
     canvas.height = 576;
 
     const renderer = new THREE.WebGLRenderer({canvas});
-    //renderer.physicallyCorrectLights = true;
+    renderer.physicallyCorrectLights = true;
     renderer.shadowMap.enabled = true;
-    renderer.shadowMap.type = THREE.PCFSoftShadowMap; // default THREE.PCFShadowMap
+    //renderer.shadowMap.type = THREE.PCFSoftShadowMap; // default THREE.PCFShadowMap
 
     const camera = new THREE.PerspectiveCamera( 50, canvas.width / canvas.height, 0.01, 1000);
-    camera.position.set(0,1,3);
+    camera.position.set(0,11,3);
     camera.rotateX(-0.2);
     scene.add(camera);
 
     const light = new THREE.PointLight();
-    light.position.set(0, 0, 1);
+    light.position.set(0, 10, 1);
     scene.add(light);
 
-    //const player = scene.getObjectByName("myAstronaut");
-    // const rightLeg = player.getObjectByName("rightLeg");
-    // const leftLeg = player.getObjectByName("leftLeg");
-    // const leftArm = player.getObjectByName("leftArm");
-    // const rightArm = player.getObjectByName("rightArm");
-    //
-    //
-    // const c = Math.PI/180;
-    // const t = 1000;
-    // const joints = [
-    //     player.position,
-    //
-    //     rightLeg.getObjectByName('Anca').rotation,
-    //     rightLeg.getObjectByName('Ginocchio').rotation,
-    //     rightLeg.getObjectByName('Caviglia').rotation,
-    //
-    //     leftLeg.getObjectByName('Anca').rotation,
-    //     leftLeg.getObjectByName('Ginocchio').rotation,
-    //     leftLeg.getObjectByName('Caviglia').rotation,
-    //
-    //     rightArm.getObjectByName('Spalla').rotation,
-    //     rightArm.getObjectByName('Gomito').rotation,
-    //
-    //     leftArm.getObjectByName('Spalla').rotation,
-    //     leftArm.getObjectByName('Gomito').rotation
-    // ];
-    // const frames = [
-    //     [ {y: 0.47}, {y: 0.51}, {y: 0.55}, {y: 0.57},       {y: 0.47}, {y: 0.51}, {y: 0.55}, {y: 0.57} ],       // Height
-    //
-    //     [ {x: 15*c}, {x: 0*c}, {x: -30*c}, {x: -60*c},      {x: -35*c}, {x: -30*c}, {x: 5*c}, {x: 15*c} ],      // rightLeg.Anca
-    //     [ {x: 30*c}, {x: 90*c}, {x: 120*c}, {x: 90*c},      {x: 0*c}, {x: 45*c}, {x: 5*c}, {x: 0*c} ],          // rightLeg.Ginocchio
-    //     [ {y: 40*c}, {y: 0*c}, {y: 30*c}, {y: 0*c},         {y: 0*c}, {y: 15*c}, {y: 10*c}, {y: -15*c} ],       // rightLeg.Caviglia
-    //
-    //     [ {x: -35*c}, {x: -30*c}, {x: 5*c}, {x: 15*c},      {x: 15*c}, {x: 0*c}, {x: -30*c}, {x: -60*c} ],      // leftLeg.Anca
-    //     [ {x: 0*c}, {x: 45*c}, {x: 5*c}, {x: 0*c},          {x: 30*c}, {x: 90*c}, {x: 120*c}, {x: 90*c} ],      // leftLeg.Ginocchio
-    //     [ {y: 0*c}, {y: 15*c}, {y: 10*c}, {y: -15*c},       {y: 40*c}, {y: 0*c}, {y: 30*c}, {y: 0*c} ],         // leftLeg.Caviglia
-    //
-    //     [ {x: -45*c}, {x: -60*c}, {x: 30*c}, {x: 45*c},     {x: 45*c}, {x: 60*c}, {x: 30*c}, {x: 30*c} ],       // rightArm.Spalla
-    //     [ {x: -90*c}, {x: -90*c}, {x: -90*c}, {x: -75*c},   {x: -45*c}, {x: -45*c}, {x: -90*c}, {x: -90*c} ],   // rightArm.Gomito
-    //
-    //     [ {x: 45*c}, {x: 60*c}, {x: 30*c}, {x: 30*c},       {x: -45*c}, {x: -60*c}, {x: 30*c}, {x: 45*c} ],     // leftArm.Spalla
-    //     [ {x: -45*c}, {x: -45*c}, {x: -90*c}, {x: -90*c},   {x: -90*c}, {x: -90*c}, {x: -90*c}, {x: -75*c} ]    // leftArm.Gomito
-    // ];
-    // const nFrames = frames[0].length;
-    //
-    //
-    //
-    // const clip = new Animation('Walk', joints, frames, ([t] * nFrames));
-    // document.getElementById("StartButton").onclick = function() {clip.Start()};
-    // document.getElementById("StopButton").onclick = function() {clip.Stop()};
-    //
-    // const idJoints = [
-    //     player.id,
-    //
-    //     rightLeg.getObjectByName('Anca').id,
-    //     rightLeg.getObjectByName('Ginocchio').id,
-    //     rightLeg.getObjectByName('Caviglia').id,
-    //
-    //     leftLeg.getObjectByName('Anca').id,
-    //     leftLeg.getObjectByName('Ginocchio').id,
-    //     leftLeg.getObjectByName('Caviglia').id,
-    //
-    //     rightArm.getObjectByName('Spalla').id,
-    //     rightArm.getObjectByName('Gomito').id,
-    //
-    //     leftArm.getObjectByName('Spalla').id,
-    //     leftArm.getObjectByName('Gomito').id
-    // ];
-    //
-    // console.log(idJoints);
-    const astronaut = new Player(scene.getObjectByName("myAstronaut"))
-    //astronaut.animations["Walk"].Start();
+    //const fabio = scene.getObjectByName("root");
+    scene.getObjectByName("Astronaut").position.set(0,10.5,0)
+    const astronaut = new Player(scene.getObjectByName("Astronaut"))
     document.getElementById("StartButton").onclick = function() {astronaut.move()};
     document.getElementById("StopButton").onclick = function() {astronaut.reset()};
 
-//     const tweens = setTweens();
-//     //tweens.forEach((tween) => tween.start());
-//
-// //  EXPERIMENTING GROUP:
-//     const group = new TWEEN.Group();
-//     tweens.forEach((tween) => group.add(tween));
-//     group.getAll().forEach((tween) => tween.start());
-//
-//     function setTweens(){
-//         const c = Math.PI/180;
-//         let tweens = [];
-//         for (let i = 0; i<joints.length; i++){
-//             let firstTween, currentTween;
-//             for (let j = 0; j < nFrames; j++){
-//                 const tween = new TWEEN.Tween(joints[i]).to(frames[i][j],t);
-//                 if (j==0) firstTween = tween;
-//                 else currentTween.chain(tween);
-//                 currentTween = tween;
-//             }
-//             currentTween.chain(firstTween);
-//             tweens.push(firstTween);
-//         }
-//         return tweens;
-//     }
-//
-
+    //const camera = scene.getObjectByName("PlayerCam");
 
     //guiOptions();
     render();
